bb9ae10dd7d2c0ee6361f808d8aea23b7ccc451e
TJ Peden <tj.peden@tj-coding.com>
2012-05-01 00:16:52 -0500

    more structure

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
-
-/**
- * Module dependencies.
- */
-
 var Express = require('express'),
     Mongoose = require('mongoose'),
     Passport = require('passport'),
     routes = require('./router');
 
-console.log(routes);
-
 require('express-resource');
 require('./passport');
-
-/*
-var routes = {};
-routes.post = require('./routes/post_controller');
-*/
 
 var app = module.exports = Express.createServer();
 var pub = __dirname + '/public';
--- router.js
+++ router.js
@@ -3,12 +3,11 @@ var fs = require('fs');
 var dir = './controllers/'
     routes = {};
 
-fs.readdir(dir, function(error, files) {
-  files.forEach(function(file) {
-    var route = require(dir + file),
-        name = file.match(/^[^_]+/);
-    routes[name] = route;
-  });
+var files = fs.readdirSync(dir);
+files.forEach(function(file) {
+  var route = require(dir + file),
+      name = file.match(/^[^_]+/);
+  routes[name] = route;
 });
 
 module.exports = routes;
